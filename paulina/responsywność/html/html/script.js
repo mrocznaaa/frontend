c5a85439bf73b54aeb9480668a4de475877a8f8b
mrocznaaa <95163952+mrocznaaa@users.noreply.github.com>
2021-11-28 13:18:55 +0100

    Delete paulina directory

--- paulina/responsywność/html/html/script.js
+++
@@ -1,9 +0,0 @@
-function testowaFunkcja() {
-  var txt;
-  if (confirm("Kliknij OK, jeśli zrozumiałeś/aś")) {
-    txt = "Brawo!";
-  } else {
-    txt = "Musisz jeszcze poćwiczyć";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
--- paulina/script.js
+++
@@ -1,9 +0,0 @@
-function testowafunkcja(){
-    var txt;
-    if (confirm("kliknij OK jeśli zrozumiałeś")){
-      txt = "Brawo!";
-    } else {
-      txt = "musisz jeszcze poćwiczyć";
-    }
-    document.getElementByld("Demo").innerHTML = txt;
-}
